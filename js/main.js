fd75b81ad2a08b979f771a71100e0de391d32636
Alessandro <drios28@outlook.es>
2021-11-02 12:31:58 -0500

    errores de registro solucionados

--- js/main.js
+++ js/main.js
@@ -6,33 +6,83 @@ let nombreUsuario = localStorage.getItem("username");
 
 let contenedor = document.createElement("div");
 
-contenedor.setAttribute("class", "contenedor-bienvenida d-flex w-100 justify-content-between");
+if (nombreUsuario) {
+    contenedor.setAttribute("class", "contenedor-bienvenida d-flex w-100 justify-content-between");
+    let bienvenidaUsuario = document.createElement("h2");
+    let horaDeEntrada = document.createElement("h2");
 
-if ((nombreUsuario.length > 0) && (nombreUsuario.length <= 20)) {
+    bienvenidaUsuario.innerHTML = `¡Bienvenido ${nombreUsuario}!`;
+    bienvenidaUsuario.setAttribute("class", "titulo__fecha");
 
-    let nombreUsuarioFinal = isNaN(nombreUsuario);
+    horaDeEntrada.innerText = "Fecha: " + new Date().toLocaleDateString();
+    horaDeEntrada.setAttribute("class", "titulo__fecha");
 
-    if (nombreUsuarioFinal == true) {
-        let bienvenidaUsuario = document.createElement("h2");
-        let horaDeEntrada = document.createElement("h2");
-
-        bienvenidaUsuario.innerHTML = `¡Bienvenido ${nombreUsuario}!`;
-        bienvenidaUsuario.setAttribute("class", "titulo__fecha");
-
-        horaDeEntrada.innerText = "Fecha: " + new Date().toLocaleDateString();
-        horaDeEntrada.setAttribute("class", "titulo__fecha");
-
-        contenedor.appendChild(bienvenidaUsuario);
-        contenedor.appendChild(horaDeEntrada);
-    } else {
-        alert("No puedes ingresar sólo números, usa también letras");
-    }
+    contenedor.appendChild(bienvenidaUsuario);
+    contenedor.appendChild(horaDeEntrada);
 
 } else {
-    alert("No ingresaste un nombre válido, inténtalo de nuevo");
+    let noIngreso = document.createElement("a");
+    noIngreso.setAttribute("class", "titulo-productos-vacio");
+    noIngreso.setAttribute("href", "registro.html");
+    noIngreso.innerText = "¡Inicie sesión o regístrese!";
+    contenedor.setAttribute("class", "contenedor-bienvenida d-flex w-100 justify-content-center");
+    contenedor.appendChild(noIngreso);
 }
 
 seccionBienvenida.appendChild(contenedor);
+
+//* CREAR LOS PRODUCTOS
+
+//! PRODUCTOS
+
+const productos = [
+    { id: "t-preparada", tipo: "sustrato", nombre: "Tierra Preparada", imagen: "../assets/img/tierras.jpeg", precio: 2, stock: 2584, vendidos: 3829 },
+    { id: "m-plastico", tipo: "maceta", nombre: "Macetas de Plástico", imagen: "../assets/img/macetas.jpeg", precio: 9, stock: 5859, vendidos: 2764 },
+    { id: "m-fibraCoco", tipo: "maceta", nombre: "Macetas de Fibra de Coco", imagen: "../assets/img/fibra-coco.jpg", precio: 12, stock: 355, vendidos: 188 },
+    { id: "m-ceramica", tipo: "maceta", nombre: "Macetas de Cerámica", imagen: "../assets/img/ceramica.jpg", precio: 10, stock: 5321, vendidos: 4779 },
+    { id: "p-hortalizas", tipo: "planta", nombre: "Plantas Hortalizas", imagen: "../assets/img/hortalizas.jpg", precio: 3, stock: 4543, vendidos: 2756 },
+    { id: "p-ornamentales", tipo: "planta", nombre: "Plantas Ornamentales", imagen: "../assets/img/ornamentales.jpg", precio: 25, stock: 4257, vendidos: 2138 },
+    { id: "p-orquideas", tipo: "planta", nombre: "Plantas Orquídeas", imagen: "../assets/img/orquideas.jpg", precio: 40, stock: 419, vendidos: 129 },
+    { id: "p-sucuCactus", tipo: "planta", nombre: "Suculentas y Cactus", imagen: "../assets/img/macetas-suculentas.jpg", precio: 8, stock: 3419, vendidos: 1289 },
+    { id: "t-musgo", tipo: "sustrato", nombre: "Sustrato - Musgo", imagen: "../assets/img/musgo.jpg", precio: 6, stock: 547, vendidos: 123 },
+    { id: "t-vermiculita", tipo: "sustrato", nombre: "Sustrato - Vermiculita", imagen: "../assets/img/vermiculita.jpg", precio: 9, stock: 257, vendidos: 33 },
+    { id: "t-perlita", tipo: "sustrato", nombre: "Sustrato - Perlita", imagen: "../assets/img/perlita.jpg", precio: 15, stock: 390, vendidos: 110 },
+    { id: "m-cemento", tipo: "maceta", nombre: "Macetas de Cemento", imagen: "../assets/img/m-cemento.jpg", precio: 4, stock: 3500, vendidos: 1540 }
+];
+
+for (const producto of productos) {
+    let cardProducto = document.createElement("div");
+    cardProducto.setAttribute("class", `producto col-12 col-md-6 col-lg-3 ${producto.tipo}`);
+    cardProducto.setAttribute("id", `${producto.id}`);
+
+    cardProducto.innerHTML = `
+    <div class="card text-center">
+        <img src="${producto.imagen}" class="producto__imagen card-img-top" alt="${producto.tipo}" />
+        <div class="card-body producto__inferior">
+            <div class="titulo">
+                <h5 class="producto__titulo card-title">${producto.nombre}</h5>
+            </div>
+            <div class="precio__container">
+                <b class="precio">S/${producto.precio}.00 la unidad</b>
+                <button class="producto__enlace boton-agregar-al-carro" id="agregar-${producto.id}">
+                <i class="producto__icon fas fa-cart-plus">
+                <b class="texto__carrito">Añadir al carrito</b>
+                </i>
+                </button>
+            </div>
+            <div>
+                <b class="stock">${producto.stock} disponibles</b><br>
+                <b class="vendidos">${producto.vendidos} vendidos</b>
+            </div>
+        </div>
+    </div>`
+
+    listaCards.push(cardProducto);
+
+    for (const card of listaCards) {
+        contenedorProductos[0].appendChild(card);
+    }
+}
 
 // * FUNCION DARK MODE
 
@@ -62,8 +112,6 @@ function activarDark() {
         tituloS.classList.toggle("texto-oscuro");
     }
 }
-
-//! DESAFÍO 09
 
 //* FILTROS (MAS VENDIDOS, MAYOR STOCK, MAYOR PRECIO Y MENOR PRECIO)
 
--- js/registro.js
+++ js/registro.js
@@ -8,18 +8,21 @@ function validarRegistro(e) {
     let usernameRegistrado = usernameRegistro.value;
     let passwordRegistrado = passwordRegistro.value;
 
-    if (isNaN(usernameRegistrado) || isNaN(passwordRegistrado)) {
-        if ((usernameRegistrado !== passwordRegistrado) && (usernameRegistrado.length > 0) && (passwordRegistrado.length > 0)) {
+    if (isNaN(usernameRegistrado) && isNaN(passwordRegistrado)) {
+        if ((usernameRegistrado !== passwordRegistrado)) {
 
-            //* ALMACENANDO USUARIO Y CONTRASEÑA EN EL LOCAL STORAGE
+            if ((usernameRegistrado.length > 5) && (passwordRegistrado.length > 5)) {
+                //* ALMACENANDO USUARIO Y CONTRASEÑA EN EL LOCAL STORAGE
 
-            localStorage.setItem("username", usernameRegistrado);
-            localStorage.setItem("password", passwordRegistrado);
+                localStorage.setItem("username", usernameRegistrado);
+                localStorage.setItem("password", passwordRegistrado);
 
-            e.preventDefault();
+                e.preventDefault();
 
-            contenedorError.innerHTML = `<b class='error d-block m-5' style='color:green;'>* ${usernameRegistrado} ¡Te has registrado con éxito! ✅</b> <a href="../pages/ingreso.html" class="boton-entrar">Iniciar Sesión</a>`
-            document.getElementById("contenedor-formulario").appendChild(contenedorError);
+                contenedorError.innerHTML = `<b class='error d-block m-5' style='color:green;'>* ${usernameRegistrado} ¡Te has registrado con éxito! ✅</b> <a href="../pages/ingreso.html" class="boton-entrar">Iniciar Sesión</a>`
+                document.getElementById("contenedor-formulario").appendChild(contenedorError);
+            }
+
         } else {
             e.preventDefault();
 
@@ -29,7 +32,7 @@ function validarRegistro(e) {
     } else {
         e.preventDefault();
 
-        contenedorError.innerHTML = "<div class='container-error'><b class='error' style='color:red;'>* ¡El nombre de usuario y la contraseña no pueden ser sólo números!</b></div>"
+        contenedorError.innerHTML = "<div class='container-error'><b class='error' style='color:red;'>* ¡El nombre de usuario o la contraseña no pueden ser sólo números!</b></div>"
         document.getElementById("contenedor-formulario").appendChild(contenedorError);
     }
 
